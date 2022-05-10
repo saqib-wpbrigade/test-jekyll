fd6b5fa97fd62d9037a950f061cf963fea3053e8
Saqib-wpbrigade <saqib.wpbrigade@gmail.com>
2022-05-10 12:23:20 +0500

    files added

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('.nav-menu')
+const btn = document.querySelectorAll('.hamburger')
+
+
+
+
+    btn.addEventListener('click', () => {
+        nav.classList.toggle('active')
+    })
+
+
